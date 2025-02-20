b4611a42b4338e7bdeaad1c05fff3b0f3cbda67a
Julie Lee <elee661@gatech.edu>
2025-02-20 00:37:25 -0500

    Fix imports

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState, ReactNode } from "react"
-import { Header } from "@/components/Header"
+import { Header } from "@/components/header"
 import Link from "next/link"
 import { useLanguage, LanguageProvider } from "@/components/LanguageProvider"
 
