db9698b9ee19862f511a38575f56886a24390533
Julie Lee <elee661@gatech.edu>
2025-02-20 00:36:49 -0500

    Add sidebar toggle for blog

---
+++ lib/blogPosts/system-design/001_chat-system.ts
@@ -0,0 +1,24 @@
+import { BlogPost } from "@/lib/types";
+
+export const systemDesignMessagingService: BlogPost = {
+    slug: "system-design/messaging-service",
+    categorySlug: "system-design",
+    title: {
+        ko: "메시징 서비스 시스템 디자인",
+        en: "Design Messaging Service",
+    },
+    date: "2025-02-19",
+    category: {
+        ko: "시스템 디자인",
+        en: "System Design",
+    },
+    description: {
+        ko: "메시징 서비스를 디자인 해봅시다",
+        en: "Let's design messaging service",
+    },
+    content: `
+## Functional Requirements
+
+  `,
+};
+
