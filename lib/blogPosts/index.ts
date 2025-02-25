f5e140dc4b65d35b1ca81393c57b2e73847aa842
Julie Lee <43656143+julielee9067@users.noreply.github.com>
2025-02-24 19:36:12 -0500

    Add elasticsearch post (#29)
    * Move event broker post to system design
    
    * Add es post
    
    ---------
    
    Co-authored-by: Julie Lee <elee661@gatech.edu>

--- lib/blogPosts/index.ts
+++ lib/blogPosts/index.ts
@@ -20,7 +20,7 @@ import {cnInternetProtocolStack} from "@/lib/blogPosts/computer-networks/003_int
 import {cnHostsAndNetworks} from "@/lib/blogPosts/computer-networks/004_interconnecting-hosts-and-networks";
 import {cnMultiplexingAndDemultiplexing} from "@/lib/blogPosts/computer-networks/005_multiplexing-and-demultiplexing";
 import {cnTCP} from "@/lib/blogPosts/computer-networks/006_tcp";
-import {csEventBrokerVsMessageBroker} from "@/lib/blogPosts/cs/003_event-broker-vs-message-broker";
+import {systemDesignEventBrokerVsMessageBroker} from "@/lib/blogPosts/system-design/002_event-broker-vs-message-broker";
 import {cnIntradomainRouting} from "@/lib/blogPosts/computer-networks/007_intradomain-routing";
 import {meetingRooms} from "@/lib/blogPosts/algorithms/010_meeting-rooms";
 import {insertInterval} from "@/lib/blogPosts/algorithms/011_insert-interval";
@@ -28,6 +28,7 @@ import {nonOverlappingIntervals} from "@/lib/blogPosts/algorithms/012_non-overla
 import {mergeIntervals} from "@/lib/blogPosts/algorithms/013_merge-intervals";
 import {employeeFreeTime} from "@/lib/blogPosts/algorithms/014_employee-free-time";
 import {meetingRoomsII} from "@/lib/blogPosts/algorithms/015_meeting-rooms-ii";
+import {systemDesignElasticsearch} from "@/lib/blogPosts/system-design/003_elastic-search";
 
 export const blogPosts: BlogPost[] = [
     csMultiprocessingAndMultiThreadingInPython,
@@ -39,8 +40,9 @@ export const blogPosts: BlogPost[] = [
     cnHostsAndNetworks,
     cnMultiplexingAndDemultiplexing,
     cnTCP,
-    csEventBrokerVsMessageBroker,
+    systemDesignEventBrokerVsMessageBroker,
     cnIntradomainRouting,
+    systemDesignElasticsearch
 ]
 
 export const leetcodeProblems: LeetCodeProblem[] = [
--- lib/blogPosts/system-design/002_event-broker-vs-message-broker.ts
+++ lib/blogPosts/system-design/002_event-broker-vs-message-broker.ts
@@ -1,16 +1,16 @@
 import { BlogPost } from "@/lib/types";
 
-export const csEventBrokerVsMessageBroker: BlogPost = {
-    slug: "cs/event-broker-vs-message-broker",
-    categorySlug: "cs",
+export const systemDesignEventBrokerVsMessageBroker: BlogPost = {
+    slug: "system-design/event-broker-vs-message-broker",
+    categorySlug: "system-design",
     title: {
         ko: "이벤트 브로커와 메시지 브로커",
         en: "Event Broker and Message Broker",
     },
     date: "2025-02-23 18:11",
     category: {
-        ko: "컴퓨터 공학",
-        en: "Computer Science",
+        ko: "시스템 디자인",
+        en: "System Design",
     },
     description: {
         ko: "이벤트 브로커와 메시지 브로커의 차이, RabbitMQ와 Kafka의 동작 원리",
