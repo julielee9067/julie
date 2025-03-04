0646145a4a320f0290151d51003467932bd85b97
Julie Lee <elee661@gatech.edu>
2025-03-04 17:39:58 -0500

    Add til

---
+++ lib/blogPosts/system-design/006_time-series-db.ts
@@ -0,0 +1,23 @@
+import { BlogPost } from "@/lib/types";
+
+export const systemDesignTimeSeriesDB: BlogPost = {
+    slug: "system-design/time-series-db",
+    categorySlug: "system-design",
+    title: {
+        ko: "시계열 데이터베이스와 TICK Stack",
+        en: "Time series DB and TICK Stack",
+    },
+    date: "2025-03-04 17:00",
+    category: {
+        ko: "시스템 디자인",
+        en: "System Design",
+    },
+    description: {
+        ko: "시계열 데이터베이스의 종류와 TICK stack",
+        en: "types of time series DB and TICK Stack",
+    },
+    content: `
+## 개요
+
+`
+};
